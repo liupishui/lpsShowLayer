9e64de076ea9ff6c73d3f73944e0cc4ee82f50e2
liupishui <liupishui@163.com>
2016-04-14 10:00:21 +0800

    Create packjson.js

---
+++ packjson.js
@@ -0,0 +1,36 @@
+{
+
+    "name": "lpsShowLayer",
+    "title": "lpsShowLayer",
+    "description": "lpsShowLayer",
+    "version": "1.0.0",
+    "homepage": "https://github.com/liupishui/lpsShowLayer",
+    "author": {
+        "name": "jingshuiyuan",
+        "url": "http://www.lpsjj.cn/blog/"
+    },
+    "repository": {
+        "type": "git",
+        "url": "https://github.com/liupishui/lpsShowLayer.git"
+    },
+    "keywords": [
+        "msgbox",
+        "messagebox"
+    ],
+    "bugs": {
+        "url": "https://github.com/liupis/lpsshowlayer/issues"
+    },
+    "dependencies": {
+        "grunt": "~0.4.1",
+        "grunt-contrib-concat": "~0.3.0",
+        "grunt-contrib-cssmin": "~0.6.0",
+        "grunt-closurecompiler": "~0.9.9",
+        "grunt-contrib-copy": "~0.4.0",
+        "grunt-transcoding": "~0.1.1",
+        "grunt-text-replace": "~0.3.9",
+        "grunt-contrib-compress": "~0.7.0"
+    },
+    "devDependencies": {
+        "grunt": "~0.4.1"
+    }
+}
